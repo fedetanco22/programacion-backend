02f9f3beac7ec3adc9dd3478872ab63ad09b2c22
Federico Tanco <fede@stagefi.com>
2023-07-28 16:50:48 -0300

    servidores con express

--- src/services/ProductManager.ts
+++ src/services/ProductManager.ts
@@ -14,7 +14,7 @@ interface Product {
     stock: number;
 }
 
-class ProductManager {
+export class ProductManager {
     path: string;
     id: number;
 
@@ -23,8 +23,20 @@ class ProductManager {
         this.id = 0;
     }
 
+    getProductId(products: string | any[]) {
+        const usersLength = products.length;
+        if (usersLength > 0) {
+            return parseInt(products[usersLength - 1].Id) + 1;
+        }
+
+        return 1;
+    }
+
     async addProduct(product: Product): Promise<void> {
+        const products = await this.getProducts();
+
         const newProduct = {
+            id: this.getProductId(products),
             title: product.title ?? 'No title',
             price: product.price ?? 0,
             thumbnail: product.thumbnail ?? 'No thumbnail',
@@ -32,42 +44,37 @@ class ProductManager {
             stock: product.stock ?? 0,
         };
 
-        const products = await this.getProducts();
         const productExists = products.some((p) => p.code === newProduct.code);
         if (productExists) {
             throw new Error('Product already exists.');
         }
 
-        products.push({ ...newProduct, id: this.id++ });
+        products.push(newProduct);
 
         try {
             await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
         } catch (error) {
-            console.log('Error al guardar el archivo', error);
+            console.log('Error when saving the file. Make', error);
         }
     }
 
     async getProducts(): Promise<Product[]> {
         try {
             const products = await fs.promises.readFile(this.path, 'utf8');
+
             return JSON.parse(products);
         } catch (err) {
             return [];
         }
     }
 
-    // getProductById(id: number): Product {
-    //     const product = this.products.find((p) => p.id === id);
-    //     if (!product) throw new Error('Product not found.');
-    //     console.log('PRODUCT', product);
-    //     return product;
-    // }
-
-    async getProductById(id: number): Promise<Product> {
+    async getProductById(id: number): Promise<Product[]> {
         const products = await this.getProducts();
-        const product = products.find((p) => p.id === id);
+
+        const product = products.filter((p) => p.id === id);
+
         if (!product) throw new Error('Product not found.');
-        console.log('PRODUCT', product);
+
         return product;
     }
 
@@ -95,42 +102,4 @@ class ProductManager {
         }
     }
 }
-
-const productManager = new ProductManager('./products.json');
-
-const runProgram = async () => {
-    // await productManager
-    //     .addProduct({
-    //         title: 'Mesa 4',
-    //         price: 12.45,
-    //         thumbnail:
-    //             'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
-    //         code: 'M89',
-    //         stock: 10,
-    //     })
-    //     .then(() => {
-    //         productManager.getProducts().then((products) => {
-    //             console.log('PRODUCTS', products);
-    //         });
-    //     });
-
-    // await productManager
-    //     .addProduct({
-    //         title: 'Regla 7',
-    //         price: 12.45,
-    //         thumbnail:
-    //             'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
-    //         code: 'R70',
-    //         stock: 10,
-    //     })
-    //     .then(() => {
-    //         productManager.getProducts().then((products) => {
-    //             console.log('PRODUCTS', products);
-    //         });
-    //     });
-
-    await productManager.deleteProduct(1);
-};
-
-runProgram();
 
